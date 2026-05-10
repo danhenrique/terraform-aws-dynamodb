6c2756e5bb42be6f21baeefc8aa6b42d162faee3
Daniel Henrique <danielhenrique.al@outlook.com>
2026-05-10 00:10:04 -0300

    fix: versions

--- versions.tf
+++ versions.tf
@@ -1,18 +1,9 @@
-provider "aws" {
-  region = "sa-east-1"
-}
-
 terraform {
-  required_version = "1.14.8"
-
+  required_version = ">= 1.14.9"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.40.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "2.7.1"
+      version = ">= 6.40.0"
     }
   }
 }
